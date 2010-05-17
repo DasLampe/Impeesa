fa96bce2138948107728fb2d7784ea0e7c65ef4f
DasLampe <daslampe@lano-crew.org>
2010-05-17 12:55:31 +0200

    Version 0.0.2.3

--- config/database.conf.php
+++ config/database.conf.php
@@ -5,6 +5,6 @@
 // +----------------------------------------------------------------------+
 define("MYSQL_HOST",	"localhost");
 define("MYSQL_USER",	"root");
-define("MYSQL_PASS",	"lanocrew-mysql_server");
+define("MYSQL_PASS",	"MySQL Passwort");
 define("MYSQL_DB",		"impeesa");
 define("MYSQL_PREFIX",	"impeesa_");
--- core/utils/IExtension.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2009 DasLampe <andre@lano-crew.org> |
-// | Encoding:  UTF-8 |
-// +----------------------------------------------------------------------+
-interface IExtension {
-
-	public function drawExtension($pageId,$contentid);
-}
